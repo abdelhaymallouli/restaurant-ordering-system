d419c10ad889909666ea220993eb047e76eeaf26
abdelhaymallouli <abdelhay.mallouli@gmail.com>
2025-10-21 10:23:40 +0100

    restaurant system

--- config.php
+++ config.php
@@ -5,7 +5,8 @@ $username = 'root';
 $password = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Correct PDO connection
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Échec de la connexion : " . $e->getMessage());
